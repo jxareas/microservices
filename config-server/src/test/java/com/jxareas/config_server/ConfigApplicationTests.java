24d7aedeae09c475f1b4dbc6f69ba7e75bc42d72
jxareas <areasjx@gmail.com>
2024-08-09 20:03:29 -0600

    Refactor top level project structure

---
+++ config-server/src/test/java/com/jxareas/config_server/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jxareas.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
